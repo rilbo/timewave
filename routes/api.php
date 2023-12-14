5e804b47b63ea468107e2b5dcd7ecccd002c130a
Valentin Muller <valmuller14@gmail.com>
2023-12-15 00:16:59 +0100

    [API] - Mise en place des routes pour les heures

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\v1\AuthController;
+use App\Http\Controllers\API\v1\TimesController;
+use App\Http\Controllers\API\v1\TravelAreaController;
+use App\Http\Controllers\API\v1\SitesNameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +18,6 @@ use App\Http\Controllers\API\v1\AuthController;
 |
 */
 
-
-
 Route::group(['prefix' => 'v1','namespace' => 'App\Http\Controllers\API\v1'], 
 function () {
     Route::group(['middleware' => 'auth:sanctum'], function () {
@@ -24,6 +25,21 @@ function () {
         Route::put("/edit-profile", [AuthController::class, 'edit']);
         Route::post("/change-password", [AuthController::class, 'updatePassword']);
         Route::delete("/logout", [AuthController::class, 'logout']);
+
+        // mes heures
+        Route::post("/time/create", [TimesController::class, 'store']);
+
+        // companies list
+        //Route::get("/companies/list", [CompaniesController::class, 'list']);
+
+        // countries list
+        //Route::get("/countries/list", [CountriesController::class, 'list']);
+
+        // trave-area list
+        Route::get("/travel-area/list", [TravelAreaController::class, 'list']);
+
+        // name site list
+        Route::get("/name-site/list", [SitesNameController::class, 'list']);
     });
 
     Route::post('/login', [AuthController::class, 'login']);
